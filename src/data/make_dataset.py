3306a18f5366a0c1c285e844c7ef057c0cc2921f
AllanSilvaP <allan.dasilvap2005@gmail.com>
2025-09-18 18:23:04 -0300

    Load_raw_data with cleaned data

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,20 +1,30 @@
 import pandas as pd
 
 def load_raw_data(path="data/raw/Telco-Customer-Churn.csv"):
+    pd.set_option('future.no_silent_downcasting', True)
     df = pd.read_csv(path)
 
     df = df.drop(columns=["customerID"])
 
+    # transform money in float
+    money_columns = ['TotalCharges', 'MonthlyCharges']
+    for column in money_columns:
+        df[column] = pd.to_numeric(df[column])
+
     ##Binary Enconding - YES AND NO
-    colums_to_change = ['Partner', 'Dependents', 'PhoneService', 'PaperlessBilling', 'Churn']
+    columns_to_change = ['Partner', 'Dependents', 'PhoneService', 'PaperlessBilling', 'Churn']
     maping = {'Yes': 1, 'No': 0}
-    for column in colums_to_change:
+    for column in columns_to_change:
         df[column] = df[column].replace(maping)
 
     ##Binary Enconding - Gender
     maping_gender = {'Female': 0, 'Male': 1}
-    df['Gender'] = df['Gender'].replace(maping_gender)
+    df['gender'] = df['gender'].replace(maping_gender)
 
+    # One Hot encondig - 3 or more options
+    colums_to_change_onehot = ['MultipleLines', 'InternetService', 'OnlineSecurity', 'OnlineBackup', 'DeviceProtection', 'TechSupport', 'StreamingTV', 'StreamingMovies', 'Contract', 'PaymentMethod']
+    for column in colums_to_change_onehot:
+        df = pd.get_dummies(df, columns=[column], dtype=int)
     print(df.head())
     return df
 
